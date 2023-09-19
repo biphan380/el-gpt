86bb51c6b468395c15ba24d4dd4a9482599684d4
biphan380 <biphan380@gmail.com>
2023-09-18 22:04:41 -0400

    chunk up both Document objects for both cases

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from llama_index import VectorStoreIndex, download_loader
 SimpleWebPageReader = download_loader("SimpleWebPageReader")
 
 loader = SimpleWebPageReader()
-documents = loader.load_data(urls=['https://www.canlii.org/en/on/laws/stat/rso-1990-c-h19/latest/rso-1990-c-h19.html?autocompleteStr=Human&autocompletePos=1#sec5_smooth'])
+# Ronald Phipps and TPSB
+documents = loader.load_data(urls=['https://www.canlii.org/en/on/onhrt/doc/2009/2009hrto877/2009hrto877.html'])
 
-index = VectorStoreIndex.from_documents(documents)
+# Hunter versus MAG
+documents2 = loader.load_data(urls=['https://www.canlii.org/en/on/onhrt/doc/2023/2023hrto1081/2023hrto1081.html'])
 
+# combine the two list[Document]
+documents.extend(documents2)
+
+# documents contains the Document object for Ronald and Betty George
+    
 with open('documents.txt', 'w') as file:
     for doc in documents:
         # Write page_content
@@ -21,3 +28,74 @@ with open('documents.txt', 'w') as file:
 
         # Add a separator between documents
         file.write("\n" + "-"*50 + "\n\n")
+
+from llama_index.text_splitter import SentenceSplitter
+
+# Here we import our SentenceSplitter to split document texts into smaller chunks, while
+# preserving paragraphs/sentences as much as possible.
+
+text_splitter = SentenceSplitter(
+    chunk_size=1024,
+    # separator=" ", ...no separator??
+)
+
+text_chunks = []
+
+# maintain relationship with source doc index, to help inject doc metadata in the next step below
+# I'm not sure how this relationship will help at the moment 
+
+'''
+So we have two Document objects in our documents list.
+If, for example, the Document at index 0 got chunked into 2 pieces, and the Document at index1 got chunked into 3 pieces
+
+doc_idxs would look like this:
+[0,0,1,1,1]
+
+and text_chunks would simply be a list that contains all 5 chunks
+
+Not sure how this helps with node creation right now, but you can check the outputs below for learning
+'''
+
+doc_idxs = []
+for doc_idx, doc in enumerate(documents):
+    print(f"currently at doc_idx number: {doc_idx}")
+    cur_text_chunks = text_splitter.split_text(doc.text)
+    text_chunks.extend(cur_text_chunks)
+    doc_idxs.extend([doc_idx] * len(cur_text_chunks))
+
+# Write doc_idxs to a file
+with open('doc_idxs.txt', 'w') as idx_file:
+    for idx in doc_idxs:
+        idx_file.write(str(idx) + '\n')
+
+# Write text_chunks to a file
+with open('text_chunks.txt', 'w') as chunk_file:
+    for chunk in text_chunks:
+        chunk_file.write(chunk)
+        chunk_file.write("\n" + "seperator_starts@@@@@@@@@@@@@@@@@@@@@@seperator_ends"*50 + "\n")  # Separator between chunks for better readability
+
+
+print(doc_idxs)
+
+# '''
+# We convert each chunk into a TextNode object, a low-level data abstraction in LlamaIndex 
+# that stores content but also allows defining metadata + relationships with other Nodes.
+
+# We inject metadata from the document into each node.
+# This essentially replicates logic in our SimpleNodePraser
+# '''
+
+# from llama_index.schema import TextNode
+
+# nodes = [] 
+
+# for idx, text_chunk in enumerate(text_chunks):
+#     node = TextNode(
+#         text=text_chunk,
+#     )
+#     src_doc = documents[doc_idxs[idx]]
+#     node.metadata = src_doc.metadata
+#     nodes.append(node)
+
+# # print a sample node
+# print(nodes[0].get_metadata_str())
