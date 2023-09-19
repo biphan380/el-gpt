7449d568adadad3b6501af6c8b31ef838799e3c7
biphan380 <biphan380@gmail.com>
2023-09-18 20:31:43 -0400

    output doc object content and metadata. Web page is one doc object with no metadata

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ loader = SimpleWebPageReader()
 documents = loader.load_data(urls=['https://www.canlii.org/en/on/laws/stat/rso-1990-c-h19/latest/rso-1990-c-h19.html?autocompleteStr=Human&autocompletePos=1#sec5_smooth'])
 
 index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query('How long do I have to bring an application before the human rights tribunal?')
-print(str(response))
+
+with open('documents.txt', 'w') as file:
+    for doc in documents:
+        # Write page_content
+        file.write("Page Content:\n")
+        file.write(doc.get_content())
+        file.write("\n\n")
+
+        # Write metadata
+        file.write("Metadata:\n")
+        for key, value in doc.metadata.items():
+            file.write(f"{key}: {value}\n")
+
+        # Add a separator between documents
+        file.write("\n" + "-"*50 + "\n\n")
