b2be4707af91d031372d576a9597c0569087caaf
biphan380 <biphan380@gmail.com>
2023-09-20 19:44:55 -0400

    inheret from BaseVectorStore to implement VectorStore2

---
+++ vector_store.py
@@ -0,0 +1,36 @@
+from typing import List, Any, Dict
+from vector_store_interface import BaseVectorStore
+from llama_index.schema import BaseNode
+
+class VectorStore2(BaseVectorStore):
+    """VectorStore2 (add/get/delete implemeneted)."""
+
+    stores_text: bool = True
+
+    def __init__(self) -> None: 
+        """Init params. """
+        self.node_dict: Dict[str, BaseNode] = {}
+
+    def get(self, text_id: str) -> List[float]:
+        """Get embedding."""
+        return self.node_dict[text_id]
+    
+    def add(
+        self,
+        nodes: List[BaseNode]
+    ) -> List[str]:
+        """Add nodes to index."""
+        for node in nodes:
+            self.node_dict[node.node_id] = node
+
+    def delete(self, node_id: str, **delete_kwargs: Any) -> None:
+        """
+        Delete nodes using with node_id.
+
+        Args:
+            node_id: str
+
+        """
+        del self.node_dict[node_id]
+
+        
